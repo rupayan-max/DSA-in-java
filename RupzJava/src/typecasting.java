bfce76df43ae1d790112a729cd57dfedc79da59f
rupayan-max <rupbera922@gmail.com>
2026-06-09 08:47:46 +0530

    Typecasting

---
+++ RupzJava/src/typecasting.java
@@ -0,0 +1,16 @@
+public class typecasting {
+    // Implicit Conversion
+    public static void main() {
+        byte num1 = 20;
+        long num2=num1;
+        System.out.println(num2);
+
+
+
+        //Explicit Conversion
+        int rubber=30;
+       short value2=(short)rubber;
+       System.out.println("Explicit value is "+value2);
+
+    }
+}
