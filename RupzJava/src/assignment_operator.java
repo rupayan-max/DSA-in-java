c9f54933086383881c0047f4620689545cf66ef0
rupayan-max <rupbera922@gmail.com>
2026-06-12 00:13:43 +0530

    Assignment Operator

---
+++ RupzJava/src/assignment_operator.java
@@ -0,0 +1,13 @@
+public class assignment_operator {
+   public static void main(String[] args) {
+    System.out.println("Assignment Operator");
+     
+
+    // Assignment Operator
+    int firstNumber= 20;
+    System.out.println(firstNumber+=20);
+    System.out.println(firstNumber-=5);
+
+   }
+
+}
