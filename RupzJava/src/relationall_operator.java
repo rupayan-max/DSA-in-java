b57293776e12fda56a20a8f65e76e4f5c58fe555
rupayan-max <rupbera922@gmail.com>
2026-06-11 07:04:10 +0530

    Relational Operator in java

---
+++ RupzJava/src/relationall_operator.java
@@ -0,0 +1,15 @@
+public class relationall_operator {
+    public static void main(String[] args) {
+        System.out.println("Relational Operator");
+        int firstvalue = 20;
+        int secondvalue = 30;
+
+        // Relational Operator
+        System.out.println(firstvalue == secondvalue); // false 
+        System.out.println(firstvalue != secondvalue); // true
+        System.out.println(firstvalue>secondvalue); // false
+        System.out.println(firstvalue<secondvalue); // true
+        System.out.println(firstvalue>=secondvalue); // false
+        System.out.println(secondvalue<=firstvalue); // false
+    }
+}
