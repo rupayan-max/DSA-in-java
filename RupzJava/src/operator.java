8bcff743a6e18f86b17d8cdfbb50ac61d5bf6499
rupayan-max <rupbera922@gmail.com>
2026-06-11 06:47:20 +0530

    Arithmetic Operator in java

---
+++ RupzJava/src/operator.java
@@ -0,0 +1,16 @@
+public class operator{
+    public static void main(String[] args) {
+        System.out.println("Opearator in java start");
+
+        // Arithmetic Operator 
+
+        int num1 = 10;
+        int num2 = 20;
+        int total = num1 + num2;
+        int min = num2-num1;
+        int multi = num1*10;
+        int dev = num2/10;
+        int student = 20;
+        System.out.println(student%3);
+    }
+}
