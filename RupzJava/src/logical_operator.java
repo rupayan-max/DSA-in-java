a924ad7aceb10ad72a3e3e672348208dbdb91e46
rupayan-max <rupbera922@gmail.com>
2026-06-11 07:16:07 +0530

    Logical Operator in java

--- RupzJava/src/logical_operator.java
+++ RupzJava/src/logical_operator.java
@@ -1,3 +1,18 @@
 public class logical_operator {
-    
+    public static void main(String[] args) {
+        System.out.println("Logical Operator");
+        
+        // Logical Operator
+
+        boolean firstvalue = true;
+        boolean secondvalue =  false;
+
+        System.out.println(firstvalue && secondvalue); // False
+        System.out.println(firstvalue&&firstvalue); // true
+        System.out.println((firstvalue == secondvalue) && (firstvalue == firstvalue)); //false
+        System.out.println((firstvalue == secondvalue) || (firstvalue == firstvalue));  // true
+        System.out.println(!firstvalue); // false
+
+
+    }
 }
