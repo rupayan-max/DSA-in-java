9270c9da6dce8c05fe40b10a62fa7847e4d7d589
rupayan-max <rupbera922@gmail.com>
2026-06-13 22:22:18 +0530

    User input using scanner class in java

---
+++ RupzJava/src/input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        // Hard Coded Values
+        int a = 90;
+        int b = 90;
+        System.out.println(a + b);
+
+        // Taking Input from user
+        Scanner sc = new Scanner(System.in); // This line make scanner class's object . Object name is sc
+        // Working of this object -> This object helps me to take input from user.
+
+        int firstuserinput = sc.nextInt();
+        System.out.println(firstuserinput);
+    }
+}
