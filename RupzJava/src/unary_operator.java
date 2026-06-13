692526fec3b3dc391c1a27e8bc926a30069e710c
rupayan-max <rupbera922@gmail.com>
2026-06-13 19:19:37 +0530

    unary Operator

---
+++ RupzJava/src/unary_operator.java
@@ -0,0 +1,17 @@
+public class unary_operator {
+    public static void main(String[] args) {
+        // Unary Operator in Java 
+        System.out.println("Unary Operator");
+
+        int newvalue= 20;
+        System.out.println(-newvalue);
+
+        // Pre-Increment
+        int newsssignvalue= 90;
+        int generatedvalue= ++newsssignvalue;
+        System.out.println(generatedvalue); // Value is 91 
+
+        // Post-Increment
+        
+    }
+}
