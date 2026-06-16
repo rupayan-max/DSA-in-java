2d89abd1d4aac27225d40085a0430465a3f631e2
rupayan-max <rupbera922@gmail.com>
2026-06-16 08:43:49 +0530

    if else Condition

---
+++ RupzJava/src/ifelse.java
@@ -0,0 +1,15 @@
+public class ifelse {
+    public static void main(String[] args) {
+        // If else condition 
+        System.out.println("If Else Condtion");
+
+        System.out.println("Please Enter a vakue --> ");
+        int mmyage= 10;
+        if (mmyage>=18){
+            System.out.println("I can Give vote");
+        }else{
+            System.out.println("Vote rejected");
+        }
+
+    }
+}
