92fba8f9e245aa82dc33e058b0a240e53fb3789d
rupayan-max <rupbera922@gmail.com>
2026-06-17 08:21:29 +0530

    Condition on ternary

--- RupzJava/src/ternary.java
+++ RupzJava/src/ternary.java
@@ -2,5 +2,9 @@ public class ternary {
     public static void main(String[] args) {
         
         System.out.println("Ternary Operator");
+        // Ternary Operator
+        int age = 34;
+        String status = (age >=18) ? "Vote" : "Can't vote";
+        System.out.println(status);
     }
 }
