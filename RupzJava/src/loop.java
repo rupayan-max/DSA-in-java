f8624e74d0c45386d2a094bb54958cabdb65d781
rupayan-max <rupbera922@gmail.com>
2026-06-19 07:20:43 +0530

    Basic For Loop in java

--- RupzJava/src/loop.java
+++ RupzJava/src/loop.java
@@ -1,8 +1,10 @@
 public class loop {
     public static void main(String[] args) {
         System.out.println("Loop in java");
-        // Loop in java 
-        
+        // Loop in java (For Loop)
+        for (int myage = 0;myage<=18;myage++){
+            System.out.println("Rupayan");
+        }
     }
     
 }
