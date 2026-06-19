dd3a8a0c30731fecc9b72ef4b725aa889a07830d
rupayan-max <rupbera922@gmail.com>
2026-06-19 07:57:34 +0530

    Pattern print using nested loop

--- RupzJava/src/loop.java
+++ RupzJava/src/loop.java
@@ -2,22 +2,20 @@ public class loop {
     public static void main(String[] args) {
         System.out.println("Loop in java");
         // Loop in java (For Loop)
-        for (int myage = 0;myage<=18;myage++){
+        for (int myage = 0; myage <= 18; myage++) {
             System.out.println("Rupayan");
         }
 
+        // Second Loop in java
 
-        // Second Loop in java 
-
-        for(int countnumber = 1 ; countnumber<=10; countnumber++){
-           System.out.println("Love Babbar");
+        for (int countnumber = 1; countnumber <= 10; countnumber++) {
+            System.out.println("Love Babbar");
         }
 
         // For Loop with 2 + increment
 
-        for(int apple = 20;apple>=1;apple = apple-2){
-            System.out.println("Apple Count is --> "+apple);
-    }
-    
+        for (int apple = 20; apple >= 1; apple = apple - 2) {
+            System.out.println("Apple Count is --> " + apple);
+        }
     }
 }
---
+++ RupzJava/src/pattern.java
@@ -0,0 +1,16 @@
+public class pattern {
+    public static void main(String[] args) {
+        System.out.println("Pattern print using nested loop");
+
+        // Pattern print through nested loop
+
+        // Basic Star pattern in java
+
+        for (int a = 1; a <= 4; a++) {
+            for (int b = 1; b <= 4; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
