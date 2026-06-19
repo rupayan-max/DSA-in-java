37fadf8f180c1deff4a1b092cae9630ac626185c
rupayan-max <rupbera922@gmail.com>
2026-06-19 07:39:10 +0530

    for loop increment value and decrement value

--- RupzJava/src/loop.java
+++ RupzJava/src/loop.java
@@ -5,6 +5,19 @@ public class loop {
         for (int myage = 0;myage<=18;myage++){
             System.out.println("Rupayan");
         }
+
+
+        // Second Loop in java 
+
+        for(int countnumber = 1 ; countnumber<=10; countnumber++){
+           System.out.println("Love Babbar");
+        }
+
+        // For Loop with 2 + increment
+
+        for(int apple = 20;apple>=1;apple = apple-2){
+            System.out.println("Apple Count is --> "+apple);
     }
     
+    }
 }
