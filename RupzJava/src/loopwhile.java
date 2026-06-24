23175fd073794eef1aff484f526d3b54f4b88146
rupayan-max <rupbera922@gmail.com>
2026-06-24 08:50:08 +0530

    issue resolved

--- RupzJava/src/loopwhile.java
+++ RupzJava/src/loopwhile.java
@@ -11,9 +11,10 @@ public class loopwhile {
         // Nested While Loop in java
 
         int i = 1;
-        int p = 1;
+       
         while (i <= 5) {
             System.out.println("Entered in First Loop");
+             int p = 1;
             while (p <= 3) {
                 System.out.println("Entered in inner loop");
                 p++;
