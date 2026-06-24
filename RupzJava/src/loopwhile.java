dfbd476689ee228f83ffe3720975fc184bfb9d25
rupayan-max <rupbera922@gmail.com>
2026-06-24 08:48:24 +0530

    nested while loop

--- RupzJava/src/loopwhile.java
+++ RupzJava/src/loopwhile.java
@@ -1,11 +1,26 @@
 public class loopwhile {
     public static void main(String[] args) {
-        // while Loop in java  
+        // while Loop in java
 
         int marks = 1;
-        while (marks<=10) {
-            System.out.println("Marks is -> "+marks);
-            marks++;            
+        while (marks <= 10) {
+            System.out.println("Marks is -> " + marks);
+            marks++;
+        }
+
+        // Nested While Loop in java
+
+        int i = 1;
+        int p = 1;
+        while (i <= 5) {
+            System.out.println("Entered in First Loop");
+            while (p <= 3) {
+                System.out.println("Entered in inner loop");
+                p++;
+            
+            }
+            System.out.println(p);
+            i++;
         }
     }
 }
