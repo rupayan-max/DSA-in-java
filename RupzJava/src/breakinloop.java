3a77dcf146c4962039e51a0621a054f93f3077f2
rupayan-max <rupbera922@gmail.com>
2026-06-24 08:09:21 +0530

    Loop in break

---
+++ RupzJava/src/breakinloop.java
@@ -0,0 +1,14 @@
+public class breakinloop {
+    public static void main(String[] args) {
+        System.out.println("Break keyword in Loop");
+
+        // Break Keyword in Loop
+
+        for (int i =1;i<=10;i++){
+            if (i==5){
+                break;
+            }
+            System.out.println("Out"+i);
+        }
+    }
+}
--- RupzJava/src/ifelseif.java
+++ RupzJava/src/ifelseif.java
@@ -1,26 +1,22 @@
 import java.util.Scanner;
 
 public class ifelseif {
-    public static void main(String[] args) {
-        System.out.println("If-Else-If Conditional statement");
-       
-         // Taking User Input
-         Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      System.out.println("If-Else-If Conditional statement");
 
-         System.out.println("Please Enter your Marks-> ");
-         int mymarks= sc.nextInt();
-         if (mymarks<=100 && mymarks>=90){
-            System.out.println("First Division");
-         } else if (mymarks >=60 && mymarks<=89){
-            System.out.println("Second Division");
-         } else if (mymarks >=40 && mymarks<=59){
-            System.out.println("Third Division");
-         } else {
-            System.out.println("Fail");
-         }
+      // Taking User Input
+      Scanner sc = new Scanner(System.in);
 
-
-
-
-    }
+      System.out.println("Please Enter your Marks-> ");
+      int mymarks = sc.nextInt();
+      if (mymarks <= 100 && mymarks >= 90) {
+         System.out.println("First Division");
+      } else if (mymarks >= 60 && mymarks <= 89) {
+         System.out.println("Second Division");
+      } else if (mymarks >= 40 && mymarks <= 59) {
+         System.out.println("Third Division");
+      } else {
+         System.out.println("Fail");
+      }
+   }
 }
