231a265cd00526589b8f6775deb6271ae7d63ab5
rupayan-max <rupbera922@gmail.com>
2026-06-24 08:21:22 +0530

    Continue statement in loop

---
+++ RupzJava/src/continuestatement.java
@@ -0,0 +1,13 @@
+public class continuestatement {
+    public static void main(String[] args) {
+        // Continue Keyword in Java
+
+        for (int i = 1; i<=10;i++){
+            if (i==8){
+                System.out.println("Skipped"+ i);
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
