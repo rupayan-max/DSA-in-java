b448f3a391ac014558c84384b08b9ef59c4dcdde
rupayan-max <rupbera922@gmail.com>
2026-06-24 08:41:53 +0530

    While loop in java

---
+++ RupzJava/src/loopwhile.java
@@ -0,0 +1,11 @@
+public class loopwhile {
+    public static void main(String[] args) {
+        // while Loop in java  
+
+        int marks = 1;
+        while (marks<=10) {
+            System.out.println("Marks is -> "+marks);
+            marks++;            
+        }
+    }
+}
