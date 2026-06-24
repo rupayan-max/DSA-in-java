39e22b7f9891cc5e48c6a26b66a2d556e0ad4e15
rupayan-max <rupbera922@gmail.com>
2026-06-25 00:58:25 +0530

    Do While Loop In Java

---
+++ RupzJava/src/dooloop.java
@@ -0,0 +1,12 @@
+public class dooloop {
+    public static void main(String[] args) {
+        System.out.println("Do While Loop In java ");
+
+        // Do while Loop in java
+        int marks = 1;
+        do {
+            System.out.println("I am In this loop");
+            marks++;
+        } while (marks <= 0); // condition False but this loop just run one time without condition checking
+    }
+}
