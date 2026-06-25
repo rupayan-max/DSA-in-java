e027a8924adb79d2e877aa0fcd26c0d3b5b8c008
rupayan-max <rupbera922@gmail.com>
2026-06-26 01:15:53 +0530

    square pattern Printing in java

--- RupzJava/src/pattern/square.java
+++ RupzJava/src/pattern/square.java
@@ -2,6 +2,12 @@ package pattern;
 
 public class square {
     public static void main(String[] args) {
-        System.out.println("n");
+        System.out.println("Square Pattern Printing  in java ");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
