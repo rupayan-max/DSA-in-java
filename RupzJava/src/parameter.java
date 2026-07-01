87d523411165a3c9daa11e400638adeeba6ba008
rupayan-max <rupbera922@gmail.com>
2026-07-02 00:07:02 +0530

    Method With Parameter and arguement

---
+++ RupzJava/src/parameter.java
@@ -0,0 +1,11 @@
+public class parameter {
+    public static void main(String[] args) {
+        // Method with Parameter
+        parameterfunc(1, 3);
+
+    }
+
+    static void parameterfunc(int a, int b) {
+        System.err.println(a + b);
+    }
+}
