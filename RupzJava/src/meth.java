cbecac2fa39b149219af9145b5100799cd0b25ad
rupayan-max <rupbera922@gmail.com>
2026-07-01 23:48:57 +0530

    First Method In Java

---
+++ RupzJava/src/meth.java
@@ -0,0 +1,12 @@
+public class meth {
+    public static void main(String[] args) {
+        // Method In Java 
+        System.out.println("Enter in First Main method");
+        rup();
+        System.out.println("Exit");
+    }
+    static void rup(){
+        System.out.println("Babbar");
+       }
+
+}
