b1a0f4c5746f3d760cf2e0234896831fcb7f29be
rupayan-max <rupbera922@gmail.com>
2026-07-04 20:49:58 +0530

    Function

--- RupzJava/src/parameter.java
+++ RupzJava/src/parameter.java
@@ -1,17 +1,18 @@
 public class parameter {
     public static void main(String[] args) {
         // Method with Parameter
-      //  parameterfunc(1, 3); // User Provided Data is Called arguement
-        int ans =multiply(2,5);
+        // parameterfunc(1, 3); // User Provided Data is Called arguement
+        int ans = multiply(2, 5);
         System.out.println(ans);
     }
 
-    static void parameterfunc(int a, int b) {  // in this part called Parameter
+    static void parameterfunc(int a, int b) { // in this part called Parameter
         System.err.println(a + b);
     }
-     static int multiply(int value1, int value2){  // Non Void Function
-         int result = value1*value2;
-         return result;
-     }
+
+    static int multiply(int value1, int value2) { // Non Void Function
+        int result = value1 * value2;
+        return result;
+    }
 
 }
