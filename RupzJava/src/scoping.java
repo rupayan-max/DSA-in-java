6f472ef772e43fa87ddf703367faceec87b93d53
rupayan-max <rupbera922@gmail.com>
2026-07-07 08:40:07 +0530

    Variable scoping

--- RupzJava/src/scoping.java
+++ RupzJava/src/scoping.java
@@ -1,3 +1,13 @@
 public class scoping {
-    
+    static int num = 1; // In class I can use anywhwere 
+    public static void main(String[] args) {
+        System.out.println(num);
+        scoping();
+    }
+
+    static void scoping(){
+        System.out.println("In");
+        int num = 1;
+        System.out.println(num);
+    }
 }
