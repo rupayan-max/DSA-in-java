38a6c934ac3b895fade0fd829c9bdadb2cc3f171
rupayan-max <rupbera922@gmail.com>
2026-07-08 08:15:10 +0530

    Array Basics

--- RupzJava/src/arr.java
+++ RupzJava/src/arr.java
@@ -1,6 +1,26 @@
 public class arr {
     public static void main(String[] args) {
-        // Array in java 
+        // Array in java
         System.out.println("Array");
+
+        // Array Creation
+
+        int arr[];
+
+        // Array Declearation
+
+        int arrr[] = new int[3];
+
+        // Initilization
+
+        int rupayan[] = { 1, 2, 3, 4, 5, 6 };
+        System.out.println(rupayan[1]);
+
+        for (int num = 0; num <= 5; num++) {
+            System.out.println("Inside of For loop");
+            System.out.println(rupayan[num]);
+            System.out.println("Outside of For loop");
+        }
+
     }
 }
