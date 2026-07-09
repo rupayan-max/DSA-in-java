affdda3d667e8e8c85560cef132379cee0c07069
rupayan-max <rupbera922@gmail.com>
2026-07-10 00:33:35 +0530

    For Each Loop

--- RupzJava/src/arr.java
+++ RupzJava/src/arr.java
@@ -13,13 +13,25 @@ public class arr {
 
         // Initilization
 
-        int rupayan[] = { 1, 2, 3, 4, 5, 6 };
-        System.out.println(rupayan[1]);
+        // int rupayan[] = { 1, 2, 3, 4, 5, 6 };
+        // System.out.println(rupayan[1]);
 
-        for (int num = 0; num <= 5; num++) {
-            System.out.println("Inside of For loop");
-            System.out.println(rupayan[num]);
-            System.out.println("Outside of For loop");
+        // for (int num = 0; num <= 5; num++) {
+        //     System.out.println("Inside of For loop");
+        //     System.out.println(rupayan[num]);
+        //     System.out.println("Outside of For loop");
+        // }
+
+
+
+
+ 
+
+     // for each loop 
+        int smit[]={10,20,30,40,50,60};
+
+        for (int namrata : smit){
+            System.out.println("Result of For Each loop ->>> "+namrata);
         }
 
     }
