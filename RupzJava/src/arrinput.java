3a3b58c77c3d61b91c42b461e19cf58695d88569
rupayan-max <rupbera922@gmail.com>
2026-07-10 00:52:04 +0530

    taking user input in array

---
+++ RupzJava/src/arrinput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class arrinput {
+    public static void main(String[] args) {
+
+        // Taking Input in array
+
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[]={};
+        int n = arr.length;
+        for (int a = 0; a <=n-1;a++){
+            System.out.println("Enter Value for array-> ");
+          int brr =  arr[a]=sc.nextInt();
+        }
+  
+
+    }
+}
