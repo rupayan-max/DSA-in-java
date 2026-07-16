4d0dd2ddfdeaaa9e26f8ace566015acea131877e
rupayan-max <rupbera922@gmail.com>
2026-07-17 00:36:39 +0530

    Array with input

--- RupzJava/src/arrinput.java
+++ RupzJava/src/arrinput.java
@@ -1,19 +1,9 @@
-import java.util.Scanner;
-
 public class arrinput {
     public static void main(String[] args) {
+        // First Array Creation
+       System.out.println("Array creation");
+       
 
-        // Taking Input in array
-
-        Scanner sc = new Scanner(System.in);
-        
-        int arr[]={};
-        int n = arr.length;
-        for (int a = 0; a <=n-1;a++){
-            System.out.println("Enter Value for array-> ");
-          int brr =  arr[a]=sc.nextInt();
-        }
-  
 
     }
 }
