50b16cda1eac662d74c9a0791203c8bcdb025efc
rupayan-max <rupbera922@gmail.com>
2026-07-17 07:43:46 +0530

    For loop apply in array

--- RupzJava/src/arrinput.java
+++ RupzJava/src/arrinput.java
@@ -1,9 +1,36 @@
 public class arrinput {
     public static void main(String[] args) {
         // First Array Creation
-       System.out.println("Array creation");
-       
+        System.out.println("Array creation");
+
+        // // Decleration
+        // int arr[];
+
+        // // Allocation
+        // int arr = new int[5];
+
+        int arr[];
+        arr = new int[5];
 
 
+        int brr[];
+        brr = new int[4];
+
+
+        int crr[] = new int[7];
+
+
+        // insert data in element 
+
+        int lovebbaar[]={76,39,20,21};
+        System.out.println(lovebbaar[3]);
+
+
+
+        // For loop in array 
+        int size = lovebbaar.length;
+        for(int i = 0; i<size; i++){
+          System.out.println(lovebbaar[i]);
+        }
     }
 }
