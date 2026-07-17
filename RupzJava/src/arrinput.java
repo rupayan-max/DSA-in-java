0e11863b36b2fca49a1f8cf2d0e7886b9a01ac7d
rupayan-max <rupbera922@gmail.com>
2026-07-17 23:24:11 +0530

    fore

--- RupzJava/src/arrinput.java
+++ RupzJava/src/arrinput.java
@@ -12,25 +12,29 @@ public class arrinput {
         int arr[];
         arr = new int[5];
 
-
         int brr[];
         brr = new int[4];
 
-
         int crr[] = new int[7];
 
+        // insert data in element
 
-        // insert data in element 
-
-        int lovebbaar[]={76,39,20,21};
+        int lovebbaar[] = { 76, 39, 20, 21 };
         System.out.println(lovebbaar[3]);
 
-
-
-        // For loop in array 
+        // For loop in array
         int size = lovebbaar.length;
-        for(int i = 0; i<size; i++){
-          System.out.println(lovebbaar[i]);
+        for (int i = 0; i < size; i++) {
+            System.out.println(lovebbaar[i]);
+        }
+
+
+        // for each loop in java 
+
+        int nrr []= {39,30,49,45,89};
+        for(int naku :nrr){
+            System.out.println("Entry");
+            System.out.println(naku);
         }
     }
 }
