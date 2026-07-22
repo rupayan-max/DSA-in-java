d5d3e0c6c408bd2de016843bd658c3a34566ad88
rupayan-max <rupbera922@gmail.com>
2026-07-22 07:27:53 +0530

    arrray sum

---
+++ RupzJava/src/arrinput2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class arrinput2 {
+    public static void main(String[] args) {
+        // First Array Initiliazation
+
+        int arr[] = { 12, 23, 34, 45 };
+
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
