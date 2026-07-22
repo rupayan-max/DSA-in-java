18c360707d38d0d8f0e313280da0312a0d15482f
rupayan-max <rupbera922@gmail.com>
2026-07-22 08:03:56 +0530

    Two sum  added

--- RupzJava/src/arrinput2.java
+++ RupzJava/src/arrinput2.java
@@ -6,8 +6,19 @@ public class arrinput2 {
 
         int arr[] = { 12, 23, 34, 45 };
 
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
-        
+        int sum = 0;
+        int len = 4;
+
+        for (int i = 0; i <= len-1; i++) {
+            
+            System.out.println(arr[i]);
+            int arrayvalues = arr[i];
+            sum =  sum+arrayvalues;
+
+            
+        }
+        System.out.println("The Sum value is -> "+sum);
     }
 }
